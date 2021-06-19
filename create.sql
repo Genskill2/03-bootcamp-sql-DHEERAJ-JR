a62f91ced7216aff3996afae0297eab9c391d64a
DHEERAJ-JR <71641472+DHEERAJ-JR@users.noreply.github.com>
2021-06-19 20:29:30 +0530

    updated create.sql

--- create.sql
+++ create.sql
@@ -1,28 +1,25 @@
-PRAGMA foreign_keys = ON;
-CREATE TABLE publisher(
-	id INTEGER PRIMARY KEY,
-	name TEXT,
-	country text
-);
+CREATE TABLE  publisher(
+  id INTEGER PRIMARY KEY,
+  name VARCHAR(255),
+  country VARCHAR(255)
+  );
 
 CREATE TABLE books(
-	id INTEGER PRIMARY KEY,
-	title TEXT,
-	publisher INTEGER,
-	CONSTRAINT fk_publisher
-	FOREIGN KEY(publisher) REFERENCES publisher(id)
-);
-
-CREATE TABLE subjects(
-	id INTEGER PRIMARY KEY,
-	name TEXT
-);
+  id INTEGER PRIMARY KEY,
+  title VARCHAR(255),
+  publisher INTEGER,
+  FOREIGN KEY(publisher) REFERENCES  publisher(id)
+  );
+  
+CREATE TABLE  subjects(
+  id INTEGER PRIMARY KEY,
+  name VARCHAR(255)
+  );
 
 CREATE TABLE books_subjects(
-	book INTEGER,
-	subject INTEGER,
-	CONSTRAINT fk_book,
-	FOREIGN KEY(book) REFERENCES books(id),
-	CONSTRAINT fk_subject
-	FOREIGN KEY(subject) REFERENCES subjects(id)
-);
+   book INTEGER,
+   subject INTEGER,
+   FOREIGN KEY(book) REFERENCES books(id),
+   FOREIGN KEY(subject) REFERENCES  subjects(id)
+  );
+  
